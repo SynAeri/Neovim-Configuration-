a3be5b22e9a1024214845e003cdaa853fe37b49e
SynAeri <Jordan.Maquiran@outlook.com>
2025-05-21 19:48:48 +1000

    modified config temp disbaling nvim

--- flake.nix
+++ flake.nix
@@ -24,6 +24,10 @@
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+          ];
+
         };
       };
     };
