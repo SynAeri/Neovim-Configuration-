32fd8b025b135beac9a84266cae4cc9cecbf7e2a
SynAeri <Jordan.Maquiran@outlook.com>
2025-05-21 21:05:01 +1000

    removed nvim config in main flake

--- flake.nix
+++ flake.nix
@@ -4,42 +4,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
-    neovim ={
-      url="github:nix-community/neovim-nightly-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     { self, nixpkgs, neovim, home-manager,  ... }:
-
-     let
-      overlayFlakeInputs = prev: final: {
-        neovim = neovim.packages.x86_64-linux.neovim;
-      };
-
-      overlayMyNeovim = prev: final: {
-        myNeovim = import ./modules/neovim/default.nix {
-          pkgs = final;
-        };
-      };
-
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [ overlayFlakeInputs overlayMyNeovim ];
-      };
-
-     in {
-
-
-      # Pkg definition
-      packages.x86_64-linux.default = neovim.packages.x86_64-linux.neovim;
-      
-      apps.x86_64-linux.default = {
-        type = "app";
-        program = "${neovim.packages.x86_64-linux.neovim}/bin/nvim";
-      };
+    {
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
--- modules/neovim/default.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs }:
-    pkgs.wrapNeovim.pkgs.neovim {
-      configure = {
-         
-      };
-};
