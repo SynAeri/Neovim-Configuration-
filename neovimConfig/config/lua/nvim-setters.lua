2699235fc406fcf91cc2f43b5bd617e02ba4fb1a
SynAeri <Jordan.Maquiran@outlook.com>
2025-05-23 00:53:52 +1000

    test

--- neovimConfig/config/lua/nvim-setters.lua
+++ neovimConfig/config/lua/nvim-setters.lua
@@ -3,7 +3,11 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.number = true
 
--- (method 2, for non lazyloaders) to load all highlights at once
- for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
-   dofile(vim.g.base46_cache .. v)
- end
+-- Only try to load base46 cache if the directory exists
+if vim.fn.isdirectory(vim.g.base46_cache) == 1 then
+  for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
+    dofile(vim.g.base46_cache .. v)
+  end
+else
+  print("base46_cache directory not found")
+end
