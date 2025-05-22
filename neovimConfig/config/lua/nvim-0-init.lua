5e22e6e8c3a86d7aff6fc388183b1e825b435452
SynAeri <Jordan.Maquiran@outlook.com>
2025-05-23 01:02:43 +1000

    removed stuff

--- neovimConfig/config/lua/nvim-0-init.lua
+++ neovimConfig/config/lua/nvim-0-init.lua
@@ -1,4 +1,1 @@
 vim.g.mapleader  = " "
-
- -- put this in your main init.lua file ( before lazy setup )
- vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46_cache/"
--- neovimConfig/config/lua/nvim-setters.lua
+++ neovimConfig/config/lua/nvim-setters.lua
@@ -3,11 +3,3 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.number = true
 
--- Only try to load base46 cache if the directory exists
-if vim.fn.isdirectory(vim.g.base46_cache) == 1 then
-  for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
-    dofile(vim.g.base46_cache .. v)
-  end
-else
-  print("base46_cache directory not found")
-end
