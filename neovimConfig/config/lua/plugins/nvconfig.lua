a50e798e6668080cc98c9f1b778066e4e120cd73
SynAeri <Jordan.Maquiran@outlook.com>
2025-05-23 01:57:47 +1000

    edited nixconfig

--- neovimConfig/config/lua/plugins/nvconfig.lua
+++ neovimConfig/config/lua/plugins/nvconfig.lua
@@ -1,104 +1,92 @@
--- config/lua/nvconfig.lua
+-- NvChad UI configuration
+local M = {}
 
-local options = {
-  base46 = {
-    theme = "onedark", -- default theme
-    hl_add = {},
-    hl_override = {},
-    integrations = {},
-    changed_themes = {},
-    transparency = false,
-    theme_toggle = { "onedark", "one_light" },
+-- UI configurations
+M.ui = {
+  theme = "onedark", -- Default theme
+
+  -- Statusline configuration
+  statusline = {
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+    separator_style = "default", -- default/round/block/arrow
   },
-  ui = {
-    cmp = {
-      icons_left = false, -- only for non-atom styles!
-      style = "default", -- default/flat_light/flat_dark/atom/atom_colored
-      abbr_maxwidth = 60,
-      -- for tailwind, css lsp etc
-      format_colors = { lsp = true, icon = "󱓻" },
-    },
-    telescope = { style = "borderless" }, -- borderless / bordered
-    statusline = {
-      enabled = true,
-      theme = "default", -- default/vscode/vscode_colored/minimal
-      -- default/round/block/arrow separators work only for default statusline theme
-      -- round and block will work for minimal theme only
-      separator_style = "default",
-      order = nil,
-      modules = nil,
-    },
-    -- lazyload it when there are 1+ buffers
-    tabufline = {
-      enabled = true,
-      lazyload = true,
-      order = { "treeOffset", "buffers", "tabs", "btns" },
-      modules = nil,
-      bufwidth = 21,
-    },
-  },
-  nvdash = {
-    load_on_startup = true,
-    header = {
-      "                            ",
-      "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
-      "   ▄▀███▄     ▄██ █████▀    ",
-      "   ██▄▀███▄   ███           ",
-      "   ███  ▀███▄ ███           ",
-      "   ███    ▀██ ███           ",
-      "   ███      ▀ ███           ",
-      "   ▀██ █████▄▀█▀▄██████▄    ",
-      "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
-      "                            ",
-      "     Powered By Neovim     ",
-      "                            ",
-    },
-    buttons = {
-      { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
-      { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
-      { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
-      { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
-      { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
-      { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
-      {
-        txt = function()
-          -- This might not work in Nix setup since we're not using lazy.nvim
-          -- Let's make it simpler
-          return "  Neovim ready!"
-        end,
-        hl = "NvDashFooter",
-        no_gap = true,
-      },
-      { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
-    },
-  },
-  term = {
-    base46_colors = true,
-    winopts = { number = false, relativenumber = false },
-    sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
-    float = {
-      relative = "editor",
-      row = 0.3,
-      col = 0.25,
-      width = 0.5,
-      height = 0.4,
-      border = "single",
-    },
-  },
-  lsp = { signature = true },
-  cheatsheet = {
-    theme = "grid", -- simple/grid
-    excluded_groups = { "terminal (t)", "autopairs", "Nvim", "Opens" }, -- can add group name or with mode
-  },
-  mason = { pkgs = {}, skip = {} },
-  colorify = {
+
+  -- Tabufline (bufferline + tabline) configuration
+  tabufline = {
     enabled = true,
-    mode = "virtual", -- fg, bg, virtual
-    virt_text = "󱓻 ",
-    highlight = { hex = true, lspvars = true },
+    lazyload = false,
+  },
+
+  -- CMP configuration
+  cmp = {
+    lspkind_text = true,
+    style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+  },
+
+  -- Telescope style
+  telescope = { style = "borderless" }, -- borderless / bordered
+}
+
+-- Base46 configurations
+M.base46 = {
+  transparency = false,
+  theme_toggle = { "onedark", "one_light" },
+}
+
+-- NvDash configuration
+M.nvdash = {
+  load_on_startup = false,
+  header = {
+    "                            ",
+"⠤⠤⠤⠤⠤⠤⢤⣄⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠤⠶⠶⠶⠦⠤⠤⠤⠤⠤⢤⣤⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀",
+"⠀⠀⠀⠀⢀⠄⢂⣠⣭⣭⣕⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠀⠀⠀⠤⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉",
+"⠀⠀⢀⠜⣳⣾⡿⠛⣿⣿⣿⣦⡠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣤⣤⣤⣤⣤⣤⣤⣤⣤⣍⣀⣦⠦⠄⣀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+"⠀⠠⣄⣽⣿⠋⠀⡰⢿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⡿⠛⠛⡿⠿⣿⣿⣿⣿⣿⣿⣷⣶⣿⣁⣂⣤⡄⠀⠀⠀⠀⠀⠀",
+"⢳⣶⣼⣿⠃⠀⢀⠧⠤⢜⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⠟⠁⠀⠀⠀⡇⠀⣀⡈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠁⠐⠀⣀⠀⠀",
+"⠀⠙⠻⣿⠀⠀⠀⠀⠀⠀⢹⣿⣿⡝⢿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡿⠋⠀⠀⠀⠀⠠⠃⠁⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⡿⠋⠀⠀",
+"⠀⠀⠀⠙⡄⠀⠀⠀⠀⠀⢸⣿⣿⡃⢼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⡏⠉⠉⠻⣿⡿⠋⠀⠀⠀⠀",
+"⠀⠀⠀⠀⢰⠀⠀⠰⡒⠊⠻⠿⠋⠐⡼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⠀⠀⠀⠀⣿⠇⠀⠀⠀⠀⠀",
+"⠀⠀⠀⠀⠸⣇⡀⠀⠑⢄⠀⠀⠀⡠⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢖⠠⠤⠤⠔⠙⠻⠿⠋⠱⡑⢄⠀⢠⠟⠀⠀⠀⠀⠀⠀",
+"⠀⠀⠀⠀⠀⠀⠈⠉⠒⠒⠻⠶⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⠀⠡⢀⡵⠃⠀⠀⠀⠀⠀⠀⠀",
+"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠦⣀⠀⠀⠀⠀⠀⢀⣤⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀",
+"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠙⠛⠓⠒⠲⠿⢍⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "                            ",
+  },
+  buttons = {
+    { txt = " Find File", keys = "ff", cmd = "Telescope find_files" },
+    { txt = "󰈔 Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+    { txt = "󰈭 Find Word", keys = "fw", cmd = "Telescope live_grep" },
+    { txt = "󱥚 Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+    { txt = "󰪛 Mappings", keys = "ch", cmd = "NvCheatsheet" },
+    { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+    {
+      txt = function()
+        local stats = require("lazy").stats()
+        local ms = math.floor(stats.startuptime) .. " ms"
+        return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+      end,
+      hl = "NvDashFooter",
+      no_gap = true,
+    },
+    { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
   },
 }
 
--- Try to load user's custom configuration (optional)
-local status, chadrc = pcall(require, "chadrc")
-return vim.tbl_deep_extend("force", options, status and chadrc or {})
+-- Terminal configuration
+M.term = {
+  base46_colors = true,
+  sizes = { sp = 0.3, vsp = 0.2 },
+}
+
+-- Cheatsheet configuration
+M.cheatsheet = {
+  theme = "grid", -- simple/grid
+}
+
+-- LSP signature configuration
+M.lsp = {
+  signature = true,
+}
+
+return M
