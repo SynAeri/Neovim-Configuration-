79cb2904e71bef9aa3f03bc35756ddbc9637a9c2
SynAeri <Jordan.Maquiran@outlook.com>
2025-05-24 02:18:04 +1000

    implemented config for neo-tree with keymap

--- neovimConfig/config/lua/plugins/nvim-neo-tree.lua
+++ neovimConfig/config/lua/plugins/nvim-neo-tree.lua
@@ -1,22 +1,17 @@
 -- config/lua/plugins/nvim-neo-tree.lua
-
-neoTree = require("neo-tree")
-
-neoTree.setup({
-    require("neo-tree").setup({
-      filesystem = {
-        follow_current_file = {
-		enabled = true
-	},
-
-        filtered_items = { hide_dotfiles = false },
-        hijack_netrw_behavior = "open_default",
-      },
-      window = {
-        width = 40,
-        focus_on_open = false,
-	position = "right",
-      },
+require("neo-tree").setup({
+  filesystem = {
+    follow_current_file = {
+      enabled = true
+    },
+    filtered_items = { hide_dotfiles = false },
+    hijack_netrw_behavior = "open_default",
+  },
+  window = {
+    width = 40,
+    focus_on_open = false,
+    position = "right",
+  },
 })
 
-vim.api.nvim_set_keymap("n", "<leader>tr", ":Neotree toggle right<CR>", { desc = "Toggle Neo-tree" })
+vim.api.nvim_set_keymap("n", "<leader>tr", ":Neotree toggle right<CR>", { noremap = true, silent = true, desc = "Toggle Neo-tree" })
