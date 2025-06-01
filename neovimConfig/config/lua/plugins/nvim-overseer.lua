34875431eac1e9c9c07d0c8de219399c39bfe459
SynAeri <Jordan.Maquiran@outlook.com>
2025-06-02 01:04:23 +1000

    added overseer compiling

--- neovimConfig/config/lua/plugins/nvim-overseer.lua
+++ neovimConfig/config/lua/plugins/nvim-overseer.lua
@@ -1,17 +1,15 @@
+-- config/lua/overseer.lua
 require('overseer').setup({
-  templates = { "builtin" },
+  templates = { "builtin" },  -- Load built-in templates
   task_list = {
     direction = "bottom",
     min_height = 25,
     max_height = 25,
   },
-
 })
 
 -- Keymaps
-vim.keymap.set('n', '<leader>oo', '<cmd>OverseerToggle<cr>')
-vim.keymap.set('n', '<leader>or', '<cmd>OverseerRun<cr>')
-vim.keymap.set('n', '<leader>oc', '<cmd>OverseerRunCmd<cr>')
-vim.keymap.set('n', '<leader>ol', '<cmd>OverseerLoadBundle<cr>')
-
-
+vim.keymap.set('n', '<leader>oo', '<cmd>OverseerToggle<cr>', { desc = "Toggle Overseer" })
+vim.keymap.set('n', '<leader>or', '<cmd>OverseerRun<cr>', { desc = "Run task" })
+vim.keymap.set('n', '<leader>oc', '<cmd>OverseerRunCmd<cr>', { desc = "Run shell command" })
+vim.keymap.set('n', '<leader>ol', '<cmd>OverseerLoadBundle<cr>', { desc = "Load task bundle" })
